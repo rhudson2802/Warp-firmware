b3f3f666436fc876e22703deeeb99df124911c88
Robert Hudson <rh689@cam.ac.uk>
2020-11-02 10:09:10 +0000

    Colour change

--- src/boot/ksdk1.1.0/devSSD1331.c
+++ src/boot/ksdk1.1.0/devSSD1331.c
@@ -161,12 +161,12 @@ devSSD1331init(void)
 	writeCommand(0x00);			//Enter start row
 	writeCommand(0x5F);			//Enter end column
 	writeCommand(0x3F);			//Enter end row
-	writeCommand(0x28d);				//Set outline to green
-	writeCommand(0x0d);
-	writeCommand(0x0d);
-	writeCommand(0x0d);				//Set fill to green
+	writeCommand(0x0d);				//Set outline to green
 	writeCommand(0x0d);
 	writeCommand(0x40d);
+	writeCommand(0x28d);				//Set fill to green
+	writeCommand(0x0d);
+	writeCommand(0x0d);
 
 
 	return 0;
