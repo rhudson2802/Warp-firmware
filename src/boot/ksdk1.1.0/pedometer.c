4e697c57512d53f041cacb8fc3dc3f126c542f84
Robert Hudson <rh689@cam.ac.uk>
2021-01-05 12:51:13 +0000

    Test 3 axis acc with low pass

--- src/boot/ksdk1.1.0/pedometer.c
+++ src/boot/ksdk1.1.0/pedometer.c
@@ -236,13 +236,13 @@ int8_t pedometer(){
 		
 		x_mean[i] = dist.x.mean;
 		x_var[i] = dist.x.variance;
-/*		
+		
 		y_mean[i] = dist.y.mean;
 		y_var[i] = dist.y.variance;
 		
 		z_mean[i] = dist.z.mean;
 		z_var[i] = dist.z.variance;
-*/		
+		
 		OSA_TimeDelay(1000);
 	};
 	
@@ -252,7 +252,7 @@ int8_t pedometer(){
 		low_pass_filter(x_mean, x_var, &low_pass, &low_pass_var, 8);
 		SEGGER_RTT_WriteString(0, "\nLow pass\n");
 		SEGGER_RTT_printf(0, "%ld\t%lu\n\n", low_pass, low_pass_var);
-/*		
+		
 		low_pass_filter(y_mean, y_var, &low_pass, &low_pass_var, 8);
 		SEGGER_RTT_WriteString(0, "\nLow pass\n");
 		SEGGER_RTT_printf(0, "%ld\t%lu\n\n", low_pass, low_pass_var);
@@ -260,7 +260,7 @@ int8_t pedometer(){
 		low_pass_filter(z_mean, z_var, &low_pass, &low_pass_var, 8);
 		SEGGER_RTT_WriteString(0, "\nLow pass\n");
 		SEGGER_RTT_printf(0, "%ld\t%lu\n\n", low_pass, low_pass_var);
-*/		
+		
 		//rotate_array_by_one(data, N);
 	//}
 	
