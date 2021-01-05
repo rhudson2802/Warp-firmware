a5db51cb2a0865653a7c91efcdf9d7b825cb9432
Robert Hudson <rh689@cam.ac.uk>
2021-01-05 16:24:19 +0000

    Build test rotate array

--- src/boot/ksdk1.1.0/pedometer.h
+++ src/boot/ksdk1.1.0/pedometer.h
@@ -18,13 +18,13 @@ typedef struct acc_measurement{
 } acc_measurement;
 
 int32_t compute_mean(int16_t data[], uint8_t N);
-uint32_t compute_variance(int16_t data[], int32_t mean, uint8_t N);
+int32_t compute_variance(int16_t data[], int32_t mean, uint8_t N);
 distribution generate_distribution(int16_t data[], uint8_t N);
 acc_measurement read_accelerometer();
-void read_acceleration_distribution(uint8_t N, int32_t * z_mean, uint32_t * z_var);
-void low_pass_filter(int32_t data[], uint32_t vars[], int32_t * output, uint32_t * out_var, uint8_t N);
+void read_acceleration_distribution(uint8_t N, int32_t * z_mean, int32_t * z_var);
+void low_pass_filter(int32_t data[], int32_t vars[], int32_t * output, int32_t * out_var, uint8_t N);
 void rotate_array_by_one(int32_t data[], uint8_t N);
 //void print_acc_data_array(acc_distribution data, uint8_t N);
-void print_array(int32_t data[], uint8_t N)
+void print_array(int32_t data[], uint8_t N);
 void print_acc_distribution(acc_distribution dist);
 int8_t pedometer();
