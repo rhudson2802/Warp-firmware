6e01f130b0aae086fa604c0f8386ee99b293a2cf
Robert Hudson <rh689@cam.ac.uk>
2021-01-06 10:49:02 +0000

    Change array size to 30

--- src/boot/ksdk1.1.0/pedometer.c
+++ src/boot/ksdk1.1.0/pedometer.c
@@ -17,7 +17,7 @@
 #include "pedometer.h"
 
 #define LOW_PASS_ORDER 5
-#define ARRAY_SIZE 100
+#define ARRAY_SIZE 30
 
 extern volatile WarpI2CDeviceState	deviceMMA8451QState;
 extern volatile uint32_t		gWarpI2cBaudRateKbps;
