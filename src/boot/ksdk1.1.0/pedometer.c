2cefeebdee8e8d662fd239f9ff8e8425593015f8
Robert Hudson <rh689@cam.ac.uk>
2021-01-07 11:09:22 +0000

    Add print statements back

--- src/boot/ksdk1.1.0/pedometer.c
+++ src/boot/ksdk1.1.0/pedometer.c
@@ -320,13 +320,13 @@ int8_t pedometer(){
 		low_pass_filter(x_mean, x_var, LOW_PASS_ORDER, low_pass_x);
 		low_pass_filter(y_mean, y_var, LOW_PASS_ORDER, low_pass_y);
 		low_pass_filter(z_mean, z_var, LOW_PASS_ORDER, low_pass_z);
-		
-/*	
+
+
 		SEGGER_RTT_printf(0, "\nX\tMEAN: %d\tVARIANCE: %d\n", low_pass_x[MEAN], low_pass_x[VAR]);
 		SEGGER_RTT_printf(0, "Y\tMEAN: %d\tVARIANCE: %d\n", low_pass_y[MEAN], low_pass_y[VAR]);
 		SEGGER_RTT_printf(0, "Z\tMEAN: %d\tVARIANCE: %d\n", low_pass_z[MEAN], low_pass_z[VAR]);
-*/	
-	
+
+
 
 		// Check if we have a new maximum
 		if (low_pass_x[MEAN] > max_x[MEAN]){
@@ -410,12 +410,12 @@ int8_t pedometer(){
 			}
 		}
 
-/*
+
 		SEGGER_RTT_printf(0, "\nX\tMAX: %d\tMIN: %d\n", max_x[MEAN], min_x[MEAN]);
 		SEGGER_RTT_printf(0, "Y\tMAX: %d\tMIN: %d\n", max_y[MEAN], min_y[MEAN]);
 		SEGGER_RTT_printf(0, "Z\tMAX: %d\tMIN: %d\n", max_z[MEAN], min_z[MEAN]);
 		SEGGER_RTT_printf(0, "\n\n\nSTEPS: %d\n\n\n", step_count);
-*/
+
 		count = (count + 1) % SAMPLE_WINDOW;
 		OSA_TimeDelay(100);
 	};
