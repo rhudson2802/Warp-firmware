6f72300d05722070582022d48faf669406836a4f
Robert Hudson <rh689@cam.ac.uk>
2021-01-08 12:43:15 +0000

    Correct mistake in max integer size

--- src/boot/ksdk1.1.0/pedometer.c
+++ src/boot/ksdk1.1.0/pedometer.c
@@ -187,13 +187,13 @@ void read_acceleration_distribution(uint8_t N, int16_t * x_mean, int16_t * x_var
 	
 	
 	if (*x_var < 0){
-		*x_var = 65534;
+		*x_var = 32767;
 	}
 	if (*y_var < 0){
-		*y_var = 65534;
+		*y_var = 32767;
 	}
 	if (*z_var < 0){
-		*z_var = 65534;
+		*z_var = 32767;
 	}
 	
 
@@ -239,7 +239,7 @@ void low_pass_filter(int16_t means[], int16_t vars[], uint8_t N, int16_t output[
 	output[VAR] = sum_vars / (N*N);
 	
 	if (output[VAR] < 0) {
-		output[VAR] = 65534;
+		output[VAR] = 32767;
 	}
 }
 
@@ -264,7 +264,7 @@ uint16_t if_variance(int16_t var1[], int16_t var2[]){
 	}
 	
 	if (uncertainty < 0){
-		uncertainty = 65534;
+		uncertainty = 32767;
 	}
 	
 	return uncertainty;
